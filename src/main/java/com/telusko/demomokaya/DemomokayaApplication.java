ac257fe78099938214bcf0ac9b21837791975de1
brian-mokaya <brianmokaya507@gmail.com>
2024-11-04 12:35:13 +0300

    added spring boot project

---
+++ src/main/java/com/telusko/demomokaya/DemomokayaApplication.java
@@ -0,0 +1,14 @@
+package com.telusko.demomokaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomokayaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemomokayaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telusko/demomokaya/DemomokayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telusko.demomokaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomokayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
